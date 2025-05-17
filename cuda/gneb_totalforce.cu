7eb9342c7d54f012ff4119565f6b36d78b139c37
Vlad Kuchkin <71226360+kuchkin@users.noreply.github.com>
2025-05-17 15:55:48 +0200

    Delete cuda directory

--- cuda/gneb_totalforce.cu
+++
@@ -1,9 +0,0 @@
-#include "reduceforce.h"
-#include "sum.h"
-
-#define load(i) src[i]
-
-extern "C" __global__ void
-totalforce(float* __restrict__ src, float*__restrict__  dst, float initVal, int n, int noi,int Nz) {
-    reduceforce(load, sum, atomicAdd,noi)
-}
