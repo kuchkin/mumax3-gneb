c24760ace2ac28a0ad5c45b0842368ec2a837751
Vlad Kuchkin <71226360+kuchkin@users.noreply.github.com>
2025-05-22 15:59:07 +0200

    Add files via upload

--- cuda/gneb_vpo4D.cu
+++ cuda/gneb_vpo4D.cu
@@ -14,100 +14,63 @@ vpo4D(float* __restrict__ mx,  float* __restrict__  my,  float* __restrict__ mz,
     
     if (i < N) {
 
-        //search direction
-        // float vf = 0.0;
-        // if((((i/(N/Nz))/(Nz/noi)) == 0 )){
-        //     vf = vf1; //first image
-        // }else if((((i/(N/Nz))/(Nz/noi)) == (noi-1) )){
-        //     vf = vf3; //last image
-        // }else{
-        //     vf = vf2; //intermadiate images
-        // }
-
-        Bx[i] = Bx[i]*vf; By[i] = By[i]*vf; Bz[i] = Bz[i]*vf; Bw[i] = Bw[i]*vf;
-
-        float3 m = {mx[i], my[i], mz[i]};
-        float3 B = {Bx[i], By[i], Bz[i]};
-
-
-        //simple
-        // m += dt*B;
-        // m = normalized(m);
-        bool p;
-        if(minend == 0 ){
-            p = true;
-            // p = ( (((i/(N/Nz))/(Nz/noi)) == 0 ) || (((i/(N/Nz))/(Nz/noi)) == (noi-1) ) )? false : true;
-        }else{
-            p = ( (((i/(N/Nz))/(Nz/noi)) == 0 ) || (((i/(N/Nz))/(Nz/noi)) == (noi-1) ) )? true : false;
-        }
-
-        //rotation rodrigues
-
-        if(p){
-
-
-        float theta = dt*len4D(B, Bw[i]);
-        float pref;
-        if(theta<=0.005){//1e-5
-            pref = 1.0 - theta*theta*(1.0-theta*theta/20.0)/6.0;
-            
-        }else{
-            pref = sin(theta)/theta;
-        }
-
-        pref *= dt;
-
         if((((i/(N/Nz))/(Nz/noi)) == 0 ) || (((i/(N/Nz))/(Nz/noi)) == (noi-1) )){
-            theta = 0.0; pref *= dt;
+
+            float nx = mx[i], ny = my[i], nz = mz[i], nv = mw[i];
+            float tx = Bx[i], ty = By[i], tz = Bz[i], tv = Bw[i];
+
+            float d1,d2,d3;
+            
+            float ss  = (nv > 0.0)? 1.0 : 1.0;
+            float den = (nv > 0.0)? 1.0/(1.0 + nv) : 1.0/(1.0 - nv);
+            
+            float g1 = nx*den, g2 = ny*den, g3 = nz*den; 
+
+            d1 = (-tx*(1.0+ss*nv - nx*nx) + ty*nx*ny + tz*nx*nz + tv*nx*(ss+nv));
+            d2 = (tx*nx*ny - ty*(1.0+ss*nv - ny*ny) + tz*ny*nz + tv*ny*(ss+nv));
+            d3 = (tx*nx*nz + ty*ny*nz - tz*(1.0+ss*nv - nz*nz) + tv*nz*(ss+nv));
+
+            g1 -= dt*d1; g2 -= dt*d2; g3 -= dt*d3;
+            
+            float gg = g1*g1+g2*g2+g3*g3;
+            float gi = 1./(1.+gg);
+            nx = 2.*g1*gi; ny = 2.*g2*gi; nz = 2.*g3*gi; nv = ss*(1.-gg)*gi;
+            mx[i] = nx;
+            my[i] = ny;
+            mz[i] = nz;
+            mw[i] = nv;
+
+        }else if(minend == 0){
+            Bx[i] = Bx[i]*vf; By[i] = By[i]*vf; Bz[i] = Bz[i]*vf; Bw[i] = Bw[i]*vf;
+
+            float3 m = {mx[i], my[i], mz[i]};
+            float3 B = {Bx[i], By[i], Bz[i]};
+
+
+            float theta = dt*len4D(B, Bw[i]);
+            float pref;
+            if(theta<=0.005){//1e-5
+                pref = 1.0 - theta*theta*(1.0-theta*theta/20.0)/6.0;
+                
+            }else{
+                pref = sin(theta)/theta;
+            }
+
+            pref *= dt;
+            
+            m = m*cos(theta) + B*pref;
+            float m4 = mw[i]*cos(theta) + Bw[i]*pref;
+
+            if(regions[i] == 255){
+                m.x = 0.0; m.y = 0.0; m.z = 1.0; m4 = 0.0;
+            }
+            float norm = 1.0/len4D(m, m4);
+            // m = normalized(m);
+                mx[i] = m.x*norm;
+                my[i] = m.y*norm;
+                mz[i] = m.z*norm;
+                mw[i] =  m4*norm;
+            }
         }
-        
-        m = m*cos(theta) + B*pref;
-        float m4 = mw[i]*cos(theta) + Bw[i]*pref;
 
-        if(regions[i] == 255){
-            m.x = 0.0; m.y = 0.0; m.z = 1.0; m4 = 0.0;
-        }
-        float norm = 1.0/len4D(m, m4);
-        // m = normalized(m);
-            mx[i] = m.x*norm;
-            my[i] = m.y*norm;
-            mz[i] = m.z*norm;
-            mw[i] =  m4*norm;
-        }
-
-        //rotation matrix
-        // float theta = dt*len(B);
-
-        // float q = cos(theta), w = 1-q;
-        // float x = -dt*Bx[i]/theta, y = -dt*By[i]/theta, z = -dt*Bz[i]/theta;
-        // float s1 = -y*z*w, s2 = x*z*w, s3 = -x*y*w;
-        // float p1 = x*sin(theta), p2 = y*sin(theta), p3 = z*sin(theta);
-
-        // float t1, t2, t3;
-        // if(theta > 1.0e-20){
-        //     t1 = (q+z*z*w) * mx[i] + (s1+p1)   * my[i] + (s2+p2)   * mz[i];
-        //     t2 = (s1-p1)   * mx[i] + (q+y*y*w) * my[i] + (s3+p3)   * mz[i];
-        //     t3 = (s2-p2)   * mx[i] + (s3-p3)   * my[i] + (q+x*x*w) * mz[i];
-        //     mx[i] = t1;
-        //     my[i] = t2;
-        //     mz[i] = t3;
-        // };
-
-        //rotation
-        // float theta = dt*len(B);
-        // if(theta < 1e-20){
-        //     m += dt*B;
-        //     m = normalized(m);
-        // }else{
-        //     float3 vecnom = cross(m,B)*(1./len(B));
-        //     m = m*cos(theta) + cross(vecnom,m)*sin(theta);
-        // }
-
-        // mx[i] = m.x;
-        // my[i] = m.y;
-        // mz[i] = m.z;
-        
-
-
-    }
 }
